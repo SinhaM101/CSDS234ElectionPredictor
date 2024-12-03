4a64ca9366753b153697ec5d7c0299cf99903a62
LukeCurran3 <lukexaviercurran@gmail.com>
2024-12-02 23:36:05 -0500

    push

--- Education_by_CountyDC.py
+++ Education_by_CountyDC.py
@@ -4,20 +4,20 @@ import pandas as pd
 
 # File Paths
 file_paths = {
-    "Arizona": "dataverse_files/Education by County/Arizona Counties Education Data.csv",
-    "Georgia": "GEORGIA DATA.csv",
-    "Michigan": "dataverse_files/Education by County/Michigan Counties Education Data.csv",
-    "Pennsylvania": "PENNSYLVANIADATA.csv",
-    "Wisconsin": "WISCONSINDATA.csv",
-    "Ohio": "dataverse_files/Education by County/Ohio Counties Education Data.csv",
-    "Nevada": "dataverse_files/Education by County/Nevada Counties Education Data.csv",
-    "Florida": "dataverse_files/Education by County/Florida Counties Education Data.csv",
+    "Arizona": "Education by County Predictive\ARIZONADATA.csv",
+    "Georgia": "Education by County Predictive/GEORGIA DATA.csv",
+    "Michigan": "Education by County Predictive/MICHIGANDATA.csv",
+    "Pennsylvania": "Education by County Predictive/PENNSYLVANIADATA.csv",
+    "Wisconsin": "Education by County Predictive/WISCONSINDATA.csv",
+    "Ohio": "Education by County Predictive/OHIODATA.csv",
+    "Nevada": "Education by County Predictive/NEVADADATA.csv",
+    "Florida": "Education by County Predictive/FLORIDADATA.csv"
 }
-
+print("HEY")
 # Function to load and clean a single file
 def load_clean_csv(file_path, state):
     # Load the file, skipping the first 5 rows
-    data = pd.read_csv(file_path, skiprows=5)
+    data = pd.read_csv(file_path, skiprows=82)
     # Remove the last 6 rows
     if len(data) > 6:  # Ensure there are enough rows to drop
         data = data.iloc[:-6]
@@ -28,21 +28,21 @@ def load_clean_csv(file_path, state):
          "Ohio", "Nevada", "Florida"
     ]
     # Remove rows where GeoName matches any state name in the exclusion list
-    data = data[~data['County'].isin(states_to_exclude)]
+    data = data[~data['county'].isin(states_to_exclude)]
 
     # Add a new column for State
     data['State'] = state  # Add the state name to a new column
 
     # Remove the last word in the County column
-    data['County'] = data['County'].str.rsplit(' ', n=1).str[0]
+    data['county'] = data['county'].str.rsplit(' ', n=1).str[0]
 
     # Adjust column order: Move the State column to the right of County
-    column_order = ['County', 'State'] + [col for col in data.columns if col not in ['County', 'State']]
+    column_order = ['county', 'State'] + [col for col in data.columns if col not in ['County', 'State']]
     data = data[column_order]
 
     # Remove rows where "GeoName" column equals "United States"
-    if 'County' in data.columns:  # Ensure the column exists
-        data = data[data['County'] != "United"]
+    if 'county' in data.columns:  # Ensure the column exists
+        data = data[data['county'] != "United"]
     return data
 
 # Process each file
